405260da25ee76336c40d61af431c0d60f3d1aa0
criscamus1 <cristobalcamus1@gmail.com>
2026-07-03 00:14:35 -0400

    entrega

--- src/main/java/com/example/usuariosAustral/config/SwaggerConfig.java
+++ src/main/java/com/example/usuariosAustral/config/SwaggerConfig.java
@@ -3,7 +3,7 @@ package com.example.usuariosAustral.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.OpenAPI;;
+import io.swagger.v3.oas.models.OpenAPI;
 
 
 @Configuration
